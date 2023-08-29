1017cba98b8594542819d35c4912a38fe8908b66
Abaresk <abaresk01@gmail.com>
2023-08-29 05:23:50 +0000

    Change site logo on lucky days

---
+++ static/js/avatar-logo.js
@@ -0,0 +1,16 @@
+import {MersenneTwister} from './mersenne-twister.js';
+
+function seedFromDate(date) {
+    return date.getFullYear() + date.getMonth() * 0x100 + date.getDate() * 0x10000;
+}
+
+function replaceLogo() {
+    const logo = document.querySelector('img');
+    logo.src = '/images/earbuds.png';
+}
+
+const rng = new MersenneTwister(seedFromDate(new Date()));
+
+if ((rng.random_int() % 32) === 0) {
+    replaceLogo();
+}
