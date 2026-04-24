eb1b97b30d32bba04dfdb4ee78269976328cebb2
Abaresk <abaresk01@gmail.com>
2026-04-23 23:15:25 -0400

    Fix footer svgs

--- flutter/lib/widgets/layout/site_footer.dart
+++ flutter/lib/widgets/layout/site_footer.dart
@@ -1,12 +1,9 @@
-import 'dart:ui_web' as ui_web;
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:web/web.dart' as web;
 import '../../theme/app_theme.dart';
 import 'constrained_body.dart';
-
-int _svgIconCounter = 0;
 
 class SiteFooter extends StatelessWidget {
   const SiteFooter({super.key});
@@ -42,7 +39,7 @@ class SiteFooter extends StatelessWidget {
   }
 }
 
-class _SocialLink extends StatelessWidget {
+class _SocialLink extends StatefulWidget {
   final String label;
   final String svgPath;
   final String url;
@@ -54,61 +51,35 @@ class _SocialLink extends StatelessWidget {
   });
 
   @override
+  State<_SocialLink> createState() => _SocialLinkState();
+}
+
+class _SocialLinkState extends State<_SocialLink> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => launchUrl(Uri.parse(url)),
-      borderRadius: BorderRadius.circular(8),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: Row(
-          children: [
-            _SvgIcon(assetPath: svgPath, size: 18),
-            const SizedBox(width: 6),
-            Text(
-              label,
-              style: const TextStyle(
-                fontSize: 14,
-                color: AppTheme.darkGray,
+    final color = _isHovered ? AppTheme.primary : AppTheme.textColor;
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: InkWell(
+        onTap: () => launchUrl(Uri.parse(widget.url)),
+        borderRadius: BorderRadius.circular(8),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                widget.svgPath,
+                width: 28,
+                height: 28,
+                colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class _SvgIcon extends StatefulWidget {
-  final String assetPath;
-  final double size;
-
-  const _SvgIcon({required this.assetPath, this.size = 18});
-
-  @override
-  State<_SvgIcon> createState() => _SvgIconState();
-}
-
-class _SvgIconState extends State<_SvgIcon> {
-  late final String _viewId;
-
-  @override
-  void initState() {
-    super.initState();
-    _viewId = 'svg-icon-${_svgIconCounter++}';
-    ui_web.platformViewRegistry.registerViewFactory(_viewId, (_) {
-      return web.HTMLImageElement()
-        ..src = '/${widget.assetPath}'
-        ..style.width = '${widget.size}px'
-        ..style.height = '${widget.size}px';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: widget.size,
-      height: widget.size,
-      child: HtmlElementView(viewType: _viewId),
     );
   }
 }
