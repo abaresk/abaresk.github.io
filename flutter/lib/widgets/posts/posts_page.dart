f9eeb2b0a8b625fb236cd1b010bf86bbd36f96c2
Abaresk <abaresk01@gmail.com>
2026-04-28 12:24:09 -0400

    Sort posts by year

--- flutter/lib/widgets/posts/posts_page.dart
+++ flutter/lib/widgets/posts/posts_page.dart
@@ -25,27 +25,44 @@ class PostsPage extends StatelessWidget {
                   return const Center(child: CircularProgressIndicator());
                 }
                 final posts = snapshot.data ?? [];
+                final Map<int, List<Post>> postsByYear = {};
+                for (final p in posts) {
+                  postsByYear.putIfAbsent(p.date.year, () => []).add(p);
+                }
+                final years = postsByYear.keys.toList()
+                  ..sort((a, b) => b.compareTo(a));
+                final widgets = <Widget>[];
+                for (final year in years) {
+                  widgets.add(const SizedBox(height: 24));
+                  widgets.add(Text(
+                    '$year',
+                    style: GoogleFonts.literata(
+                        fontSize: 24,
+                        color: AppTheme.textColor,
+                        fontWeight: FontWeight.w500),
+                  ));
+                  widgets.add(const SizedBox(height: 8));
+                  for (final p in postsByYear[year]!) {
+                    widgets.add(Row(
+                      children: [
+                        Expanded(
+                          child: TabbedLinkCard(
+                            title: p.title,
+                            route: '/posts/${p.slug}',
+                          ),
+                        ),
+                        Text(
+                          p.monthDay,
+                          style: GoogleFonts.literata(
+                              fontSize: 13, color: AppTheme.darkGray),
+                        ),
+                      ],
+                    ));
+                  }
+                }
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: posts
-                      .map(
-                        (p) => Row(
-                          children: [
-                            Expanded(
-                              child: TabbedLinkCard(
-                                title: p.title,
-                                route: '/posts/${p.slug}',
-                              ),
-                            ),
-                            Text(
-                              p.monthDay,
-                              style: GoogleFonts.literata(
-                                  fontSize: 13, color: AppTheme.darkGray),
-                            ),
-                          ],
-                        ),
-                      )
-                      .toList(),
+                  children: widgets,
                 );
               },
             ),
