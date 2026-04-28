2b662c0c1837956c0230379341f81be3a24af8cc
Abaresk <abaresk01@gmail.com>
2026-04-28 09:56:17 -0400

    Remove # from URLs

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const BlogApp());
 }
